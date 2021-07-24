e98b3b41771b7016011e8f2527cd0e35888903d4
Cesar Parrado <cesarparrado970@gmail.com>
2021-07-24 11:30:21 -0500

    feat: docstring de la funcion generic_model_mutation_process

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,15 @@
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    """
+    Permite crear o actualizar una instancia de un modelo en específico,
+    si el parámetro commit es verdadero el cambio se vera reflejado en la
+    base de datos, de lo contrario solamente quedara en el objeto generado.
+
+    :param model: clase del modelo del cual se va a generar la instancia
+    :param data: información de los campos a modificar en la instancia
+    :param id: identificador del registro a actualizar (solamente para actualización)
+    :param commit: boleano que especifica si los cambios se guardaran en la base de datos
+    :return: una instancia del modelo con la información actualizada
+    """
     if id:
         item = model.objects.get(id=id)
         try:
